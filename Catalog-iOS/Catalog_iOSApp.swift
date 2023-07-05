ca01ad208404e65cd282d55b82847bbe6d85afe0
septiadi70 <septiadi70@gmail.com>
2023-07-05 14:40:57 +0700

    Initial commit

---
+++ Catalog-iOS/Catalog_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Catalog_iOSApp.swift
+//  Catalog-iOS
+//
+//  Created by Andi Septiadi on 05/07/23.
+//
+
+import SwiftUI
+
+@main
+struct Catalog_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
